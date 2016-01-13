f62bdc5e76a87be9aae7f3b8ff6a52f86bb22e84
metallica <carlos.thrashaholic@gmail.co>
2016-01-13 11:39:23 -0600

    haber si se puede hacer la base en el host

--- sistema_dietas/settings.py
+++ sistema_dietas/settings.py
@@ -5,33 +5,27 @@ import pymysql
 
 pymysql.install_as_MySQLdb()
 
-'''
+#'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_karussa',
-        'HOST': '162.243.186.32',
-        'USER': 'dev',
-        'PASSWORD': 'Rd3nvK$',
-        'PORT': '3306',
-    },
-    'db_videos': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'test_videos',
-        'HOST': '162.243.186.32',
-        'USER': 'dev',
-        'PASSWORD': 'Rd3nvK$',
+        'NAME': 'thrashformer$db_eds',
+        'HOST': 'thrashformer.mysql.pythonanywhere-services.com',
+        'USER': 'thrashformer',
+        'PASSWORD': 'elnanoz1!',
         'PORT': '3306',
     }
 }
-#'''
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db_karussa.sqlite3'),
     }
 }
+
+#'''
+
 
 NOMBRE_LOG = "local"
 DIRECCION_BASE = 'http://localhost:8000/'
